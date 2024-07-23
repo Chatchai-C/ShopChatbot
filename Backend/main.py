bd650999af0a1b1923b3a826db6beaf6db5fe961
J4cX <chatchai.cjk@gmail.com>
2024-07-23 14:57:19 +0700

    update backend to fetch data from mysql

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-
+import db_sql
 app = FastAPI()
 
 @app.post("/")
@@ -15,6 +15,20 @@ async def handle_request(request: Request):
     output_contexts = payload['queryResult']['outputContexts']
 
     if intent == "Tracking.Order.Num - context ongoing-tracking":
-        return JSONResponse(content={
-            "fulfillmentText": f"Received {intent} in the backend"
-        })
+        response = track_order(parameters)
+        return response 
+        
+
+
+def track_order(parameters: dict):
+    order_id = int(parameters['number'])
+    order_status = db_sql.get_order_status(order_id)
+
+    if order_status:
+        fulfillment_text = f"หมายเลข {order_id} มีสถานะ {order_status}"
+    else :
+        fulfillment_text = f"ไม่มีหมายเลข {order_id}"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
