0393bb1bf583b32d2a9c2f97221c0fb99db29747
J4cX <chatchai.cjk@gmail.com>
2024-07-23 16:56:47 +0700

    update to extend item

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import db_sql
+import session_iden
+
 app = FastAPI()
+inprogress_orders = {}
 
 @app.post("/")
 async def handle_request(request: Request):
@@ -14,6 +17,8 @@ async def handle_request(request: Request):
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
 
+    session_id = session_iden.extract_session_id(output_contexts[0]['name'])
+
     intent_handler_dict = {
         'Add.Order - context ongoing-order': add_to_order,
         #'Remove.Order - context ongoing-order': remove_from_order,
@@ -21,17 +26,26 @@ async def handle_request(request: Request):
         'Tracking.Order.Num - context ongoing-tracking': track_order
     }
 
-    return intent_handler_dict[intent](parameters)
+    return intent_handler_dict[intent](parameters, session_id)
 
-def add_to_order(parameters: dict):
+def add_to_order(parameters: dict,session_id: str):
     item = parameters["Item"]
     quantities = parameters["number"]
 
     if len(item) != len(quantities):
-        fulfillment_text = f"ขอโทษทีค่ะ ฉันไม่เข้าใจสิ่งที่คุณกำลังอธิบาย กรุณาบอกชื่อสินค้าและจำนวนชิ้นด้วยค่ะ"
+        fulfillment_text = f"ขอโทษทีค่ะ ฉันไม่เข้าใจสิ่งที่คุณกำลังอธิบาย กรุณาบอกชื่อสินค้าและจำนวนชิ้นให้ชัดเจนด้วยค่ะ"
     else:
-        fulfillment_text = f"{item} และ {quantities} in backend"
-    
+        new_item = dict(zip(item, quantities))
+
+        if session_id in inprogress_orders:
+            current_item = inprogress_orders[session_id]
+            current_item.update(new_item)
+            inprogress_orders[session_id] = current_item
+        else:
+            inprogress_orders[session_id] = new_item
+
+        order_str = session_iden.get_str_from_food_dict(inprogress_orders[session_id])
+        fulfillment_text = f"ตอนนี้ในคำสั่งซื้อมีสิ้นค้าดังนี้ {order_str} คุณต้องการอะไรเพิ่มหรือไม่"
     return JSONResponse(content={
         "fulfillmentText": fulfillment_text
     })
